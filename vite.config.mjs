36f3709492348abced09ab5a51daa79d20d465ba
anish-666 <tubay.com@gmail.com>
2025-08-28 21:27:15 +0530

    Update and rename vite.config.js to vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,9 @@
+// vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: { outDir: 'dist' },
+  base: '/' // keep SPA base at root
 })
